23eba596cdca7e6d053180eec01286763d5de486
Kevin Kondratuk <paintface07@gmail.com>
2024-12-05 21:44:15 -0600

    working day 1

--- days/1/main_test.ts
+++ days/1/main_test.ts
@@ -1,6 +1,24 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import {calculateDiffSum} from "./main.ts";
 
-Deno.test(function addTest() {
-    assertEquals(add(2, 3), 5);
+Deno.test(function negativeDiffSums() {
+    const data: string = `
+1   -2
+3   1
+2   -1
+-100   10
+`
+    assertEquals(calculateDiffSum(data), 108);
+});
+
+Deno.test(function example() {
+    const data: string = `
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+`
+    assertEquals(calculateDiffSum(data), 11);
 });
